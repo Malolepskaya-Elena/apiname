c4dc08d1f08ff3ea4ac5fa34a0533ece91dca3f3
Елена Малолепская <03054177@mail.ru>
2022-04-21 13:11:56 +0300

    Rework

--- src/main/java/ru/netology/apiname/Post.java
+++ src/main/java/ru/netology/apiname/Post.java
@@ -7,7 +7,7 @@ public class Post {
     public String patronymic;
     public String phone;
     public String surname;
-    public Boolean subscription;
     public FormDate birthday;
-
+    public boolean subscription;
+    
 }
